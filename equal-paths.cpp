b1418b9a753b2373c14acde10b7f72863356abcb
Rowena Chen <rowenach@usc.edu>
2025-11-11 02:41:10 +0000

    Implement equalPaths function

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,5 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
+#include<algorithm>
 
 #endif
 
@@ -8,11 +9,41 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int getDepth(Node* root);
+
+int getDepth(Node* root){
+  if(root == nullptr){
+    return 0;
+  }
+  return 1+ max(getDepth(root -> left),getDepth(root -> right)) ;
+}
+
 
 
 bool equalPaths(Node * root)
 {
     // Add your code below
 
+  if(root == nullptr){
+    return true;
+  }
+
+  if(root -> left == nullptr && root -> right == nullptr){
+    return true;
+  }
+
+  int leftDepth = getDepth(root -> left);
+  int rightDepth = getDepth(root -> right);
+
+  if(root -> left && root -> right && leftDepth!=rightDepth){
+    return false;
+  }
+
+  return equalPaths(root ->left) && equalPaths(root ->right);
+
 }
+
+
+
+
 
